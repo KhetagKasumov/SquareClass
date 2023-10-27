071696b1908fb7d94d2bc520250942c523dd0ff5
KhetagKasumov <khetagkasumov@gmail.com>
2023-10-27 11:49:59 +0300

    Код находится в ветке main

--- Demo/main.cpp
+++ Demo/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include "../Square/Point.h"
-
+// я случайно отправил код в main и не могу отправить его на проверку. Весь код лежит в основной ветке
 int main()
 {
 	Point a{ 1,2 };
